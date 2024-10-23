6b27af0811159a4eb881ce37266ed701583bf736
rajeswari-Machina <rajeswarimachina02@gmail.com>
2024-10-23 10:01:01 +0530

    Added folder structure

---
+++ FRS-Client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+/**
+ * The main component of the application.
+ * 
+ * @returns {JSX.Element} The rendered App component.
+ */
+const App=()=>{
+  return(
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/admin" element={<Admin />} />
+{/* <Route path="/student" element={<Student />} />
+<Route path="/faculty" element={<Faculty />} /> */}
+    </Routes>  
+  )
+}

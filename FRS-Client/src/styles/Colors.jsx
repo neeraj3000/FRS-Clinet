e35896e2d90ac995922304f1371a14c40eda861f
abhishekKappala <abhishekbunny.001@gmail.com>
2024-10-25 15:29:30 +0530

    Login page created

---
+++ FRS-Client/src/styles/Colors.jsx
@@ -0,0 +1,9 @@
+// src/colors.js
+export const Colors = {
+    backgroundColor: "#E3F2FD", // Light Blue
+    headerBackground: "#BBDEFB", // Darker Blue
+    textColor: "#000000", // Black
+    buttonColor: "#2196F3", // Blue
+    buttonTextColor: "#FFFFFF", // White
+    filterBoxBackground: "#E1F5FE", // Light Grayish Blue
+};
